4f8148f2c664dba84728876d35f811e13b1b16b5
amar2468 <plakaloam@gmail.com>
2024-02-15 15:29:11 +0000

    Added landing page controller, which will use react to display the landing page.

---
+++ applitrackbackend/src/main/java/com/organisation/applitrackbackend/LandingPage/LandingPageController.java
@@ -0,0 +1,13 @@
+package com.organisation.applitrackbackend.LandingPage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LandingPageController {
+    @GetMapping("/")
+    public String landing_page()
+    {
+        return "This is the landing page. I will use react for the new landing page";
+    }
+}
