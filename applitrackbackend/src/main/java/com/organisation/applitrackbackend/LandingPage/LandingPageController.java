725f4afd65a394eb774ff0ce861d1b5089d86a27
amar2468 <plakaloam@gmail.com>
2024-02-16 16:35:51 +0000

    Added RequestMapping to the Landing page controller

--- applitrackbackend/src/main/java/com/organisation/applitrackbackend/LandingPage/LandingPageController.java
+++ applitrackbackend/src/main/java/com/organisation/applitrackbackend/LandingPage/LandingPageController.java
@@ -1,9 +1,11 @@
 package com.organisation.applitrackbackend.LandingPage;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class LandingPageController {
     @GetMapping("/")
     public String landing_page()
