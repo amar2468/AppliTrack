6442cdf16b8ef6f06c2a3d58977bfd345ffb02a9
amar2468 <plakaloam@gmail.com>
2024-02-16 16:19:45 +0000

    Created POST, DELETE, and GET mappings to add a user to the database, remove a user, and view all the users in the database. PUT functionality will also be implemented.

--- applitrackbackend/src/main/java/com/organisation/applitrackbackend/User/UserController.java
+++ applitrackbackend/src/main/java/com/organisation/applitrackbackend/User/UserController.java
@@ -1,4 +1,36 @@
 package com.organisation.applitrackbackend.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/sign_up")
+    public void signUp(@RequestBody User user)
+    {
+        userService.signUp(user);
+    }
+
+    @GetMapping("/all_users")
+    public List<User> getAllUsers()
+    {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/delete_user/{user_id}")
+    public void deleteUserAccount(@PathVariable("user_id") Long id_user)
+    {
+        userService.deleteUserAccount(id_user);
+    }
 }
