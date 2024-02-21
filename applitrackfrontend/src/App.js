a2680771091d872f6467b974503e1876cf16f94f
amar2468 <plakaloam@gmail.com>
2024-02-21 17:06:04 +0000

    Implemented react router functionality, starting with the landing page, which will be loaded when the user access the root url.

--- applitrackfrontend/src/App.js
+++ applitrackfrontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      AppliTrack
-    </div>
+    <Router>
+        <Routes>
+            <Route exact path="/" element={<LandingPage />} />
+        </Routes>
+    </Router>
   );
 }
 
