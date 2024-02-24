012a8946bc2bc31d7eed2cf17870c75c685ffa83
amar2468 <plakaloam@gmail.com>
2024-02-24 16:28:45 +0000

    Added two routes to handle "sign up" and "sign in" functionality

--- applitrackfrontend/src/App.js
+++ applitrackfrontend/src/App.js
@@ -6,12 +6,16 @@ import {
 } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
         <Routes>
             <Route exact path="/" element={<LandingPage />} />
+            <Route exact path="/sign_up" element={<SignUp />} />
+            <Route exact path="/sign_in" element={<SignIn />} />
         </Routes>
     </Router>
   );
