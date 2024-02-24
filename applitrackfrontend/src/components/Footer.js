a24a8b5a55f369514d3962608e0fc92855b138e3
amar2468 <plakaloam@gmail.com>
2024-02-24 16:31:07 +0000

    Removed some unnecessary links and changed the order of some elements

--- applitrackfrontend/src/components/Footer.js
+++ applitrackfrontend/src/components/Footer.js
@@ -1,18 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/X';
 
 const logoStyle = {
-  width: '100px',
+  width: '150px',
   height: 'auto',
 };
 
@@ -20,11 +18,24 @@ function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" mt={1}>
       {'Copyright © '}
-      <Link href="#">AppliTrack&nbsp;</Link>
+      <Link href="/">AppliTrack&nbsp;</Link>
       {new Date().getFullYear()}
     </Typography>
   );
 }
+
+const scrollToSection = (sectionId) => {
+  const sectionElement = document.getElementById(sectionId);
+  const offset = 128;
+  if (sectionElement) {
+    const targetScroll = sectionElement.offsetTop - offset;
+    sectionElement.scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({
+      top: targetScroll,
+      behavior: 'smooth',
+    });
+  }
+};
 
 export default function Footer() {
   return (
@@ -64,30 +75,6 @@ export default function Footer() {
                 alt="logo of applitrack"
               />
             </Box>
-            <Typography variant="body2" fontWeight={600} gutterBottom>
-              Newsletter
-            </Typography>
-            <Typography variant="body2" color="text.secondary" mb={2}>
-              Subscribe to our newsletter for weekly updates and promotions.
-            </Typography>
-            <Stack direction="row" spacing={1} useFlexGap>
-              <TextField
-                id="outlined-basic"
-                hiddenLabel
-                size="small"
-                variant="outlined"
-                fullWidth
-                aria-label="Enter your email address"
-                placeholder="Your email address"
-                inputProps={{
-                  autocomplete: 'off',
-                  ariaLabel: 'Enter your email address',
-                }}
-              />
-              <Button variant="contained" color="primary" sx={{ flexShrink: 0 }}>
-                Subscribe
-              </Button>
-            </Stack>
           </Box>
         </Box>
         <Box
@@ -100,13 +87,10 @@ export default function Footer() {
           <Typography variant="body2" fontWeight={600}>
             Product
           </Typography>
-          <Link color="text.secondary" href="#">
+          <Link color="text.secondary" onClick={() => scrollToSection('features')}>
             Features
           </Link>
-          <Link color="text.secondary" href="#">
-            Testimonials
-          </Link>
-          <Link color="text.secondary" href="#">
+          <Link color="text.secondary" onClick={() => scrollToSection('faq')}>
             FAQs
           </Link>
         </Box>
@@ -124,82 +108,39 @@ export default function Footer() {
             About us
           </Link>
           <Link color="text.secondary" href="#">
-            Careers
-          </Link>
-          <Link color="text.secondary" href="#">
-            Press
-          </Link>
-        </Box>
-        <Box
-          sx={{
-            display: { xs: 'none', sm: 'flex' },
-            flexDirection: 'column',
-            gap: 1,
-          }}
-        >
-          <Typography variant="body2" fontWeight={600}>
-            Legal
-          </Typography>
-          <Link color="text.secondary" href="#">
-            Terms
-          </Link>
-          <Link color="text.secondary" href="#">
-            Privacy
-          </Link>
-          <Link color="text.secondary" href="#">
-            Contact
+            Contact Us
           </Link>
         </Box>
       </Box>
-      <Box
+      <div>
+        <Copyright />
+      </div>
+      <Stack
+        direction="row"
+        justifyContent="left"
+        spacing={1}
+        useFlexGap
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          pt: { xs: 4, sm: 8 },
-          width: '100%',
-          borderTop: '1px solid',
-          borderColor: 'divider',
+          color: 'text.secondary',
         }}
       >
-        <div>
-          <Link color="text.secondary" href="#">
-            Privacy Policy
-          </Link>
-          <Typography display="inline" sx={{ mx: 0.5, opacity: 0.5 }}>
-            &nbsp;•&nbsp;
-          </Typography>
-          <Link color="text.secondary" href="#">
-            Terms of Service
-          </Link>
-          <Copyright />
-        </div>
-        <Stack
-          direction="row"
-          justifyContent="left"
-          spacing={1}
-          useFlexGap
-          sx={{
-            color: 'text.secondary',
-          }}
+        <IconButton
+          color="inherit"
+          href="#"
+          aria-label="X"
+          sx={{ alignSelf: 'center' }}
         >
-          <IconButton
-            color="inherit"
-            href="#"
-            aria-label="X"
-            sx={{ alignSelf: 'center' }}
-          >
-            <TwitterIcon />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            href="#"
-            aria-label="LinkedIn"
-            sx={{ alignSelf: 'center' }}
-          >
-            <LinkedInIcon />
-          </IconButton>
-        </Stack>
-      </Box>
+          <TwitterIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          href="#"
+          aria-label="LinkedIn"
+          sx={{ alignSelf: 'center' }}
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </Stack>
     </Container>
   );
 }
